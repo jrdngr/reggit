8dec316c50f8203445950cd53538a167aa78e0da
Jordan <jordan.t.grace@gmail.com>
2021-01-08 01:38:26 +0000

    Added credential helper example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use git2::{Cred, Config, Error, RemoteCallbacks};
+use std::env;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+  // Prepare callbacks.
+  let mut callbacks = RemoteCallbacks::new();
+  callbacks.credentials(|url, _username_from_url, _allowed_types| {
+    Cred::credential_helper(
+      &Config::open_default()?,
+      url,
+      None,
+    )
+  });
+
+  // Prepare fetch options.
+  let mut fo = git2::FetchOptions::new();
+  fo.remote_callbacks(callbacks);
+
+  // Prepare builder.
+  let mut builder = git2::build::RepoBuilder::new();
+  builder.fetch_options(fo);
+
+  // Clone the project.
+  builder.clone(
+    "git@github.com:picklenerd/reggit.git",
+    Path::new("./reggit"),
+  );
 }
